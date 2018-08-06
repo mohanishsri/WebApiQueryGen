586667d56fcc54dd0647d7f95ee8f138218ab545
mohanish.sri <mohanish.sri@gmail.com>
2018-08-06 14:30:11 +0100

    updated with service

---
+++ WebAPI/Controllers/addattributeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+using System.Web.Http.Cors;
+
+namespace WebAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class addattributeController : ApiController
+    {
+        AddNewAttribute db = new AddNewAttribute();
+        [HttpGet]
+        [Route("api/addattribute/Index")]
+        public IEnumerable<attributecolval> Get()
+        {
+            return db.GetColumnValues();
+        }
+
+        // GET api/recipe
+        [HttpGet]
+        [Route("api/addattribute/Index")]
+        public IEnumerable<attributecolval> Get(string colname)
+        {
+            return db.GetColumnValues(colname);
+        }
+
+        // GET api/addattribute/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/addattribute
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/addattribute/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/addattribute/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAPI/Models/AddNewAttribute.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class AddNewAttribute
+    {
+        string connectionString =
+         "Data Source=apsgs-ctl01,17001;Initial Catalog=DEV_SIP_STG1;"
+         + "Integrated Security=true";
+
+        public IEnumerable<attributecolval> GetColumnValues(string colname)
+        {
+            IList<attributecolval> lstreceipe = new List<attributecolval>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetColumnValues"))
+                {
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@colname", colname);
+
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        lstreceipe = dt.DataTableToList<attributecolval>();
+                    }
+                }
+            }
+
+            return lstreceipe;
+        }
+
+        public IEnumerable<attributecolval> GetColumnValues()
+        {
+            string colname = "Diag_01_Derived";
+
+            IList<attributecolval> lstreceipe = new List<attributecolval>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetColumnValues"))
+                {
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@colname", colname);
+
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        lstreceipe = dt.DataTableToList<attributecolval>();
+                    }
+                }
+            }
+
+            return lstreceipe;
+        }
+
+    }
+}
