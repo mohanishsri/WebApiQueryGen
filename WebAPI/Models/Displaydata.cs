b30a47bbe1a2daa3f33f63e22bd870586e9a1090
mohanish.sri <mohanish.sri@gmail.com>
2018-08-06 11:07:36 +0100

    modified return type to array

--- WebAPI/Models/Displaydata.cs
+++ WebAPI/Models/Displaydata.cs
@@ -10,40 +10,40 @@ namespace WebAPI.Models
         public string AGEGROUP_Derived { get; set; }
         public string DIAG_01_Derived { get; set; }
         public string DIAG_02_Derived { get; set; }
-        public int DIAG_03_Derived { get; set; }
-        public int DIAG_04_Derived { get; set; }
-        public int DIAG_05_Derived { get; set; }
-        public int DIAG_06_Derived { get; set; }
-        public int DIAG_07_Derived { get; set; }
-        public int DIAG_08_Derived { get; set; }
-        public int DIAG_09_Derived { get; set; }
-        public int DIAG_10_Derived { get; set; }
-        public int DIAG_11_Derived { get; set; }
-        public int DIAG_12_Derived { get; set; }
-        public int OPERTN_01_Derived { get; set; }
-        public int OPERTN_02_Derived { get; set; }
-        public int OPERTN_03_Derived { get; set; }
-        public int OPERTN_04_Derived { get; set; }
-        public int OPERTN_05_Derived { get; set; }
-        public int OPERTN_06_Derived { get; set; }
-        public int OPERTN_07_Derived { get; set; }
-        public int OPERTN_08_Derived { get; set; }
-        public int OPERTN_09_Derived { get; set; }
-        public int OPERTN_10_Derived { get; set; }
-        public int OPERTN_11_Derived { get; set; }
-        public int OPERTN_12_Derived { get; set; }
-        public int OPERTN_13_Derived { get; set; }
-        public int OPERTN_14_Derived { get; set; }
-        public int OPERTN_15_Derived { get; set; }
-        public int OPERTN_16_Derived { get; set; }
-        public int OPERTN_17_Derived { get; set; }
-        public int OPERTN_18_Derived { get; set; }
-        public int OPERTN_19_Derived { get; set; }
-        public int OPERTN_20_Derived { get; set; }
-        public int OPERTN_21_Derived { get; set; }
-        public int OPERTN_22_Derived { get; set; }
-        public int OPERTN_23_Derived { get; set; }
-        public int OPERTN_24_Derived { get; set; }
+        public string DIAG_03_Derived { get; set; }
+        public string DIAG_04_Derived { get; set; }
+        public string DIAG_05_Derived { get; set; }
+        public string DIAG_06_Derived { get; set; }
+        public string DIAG_07_Derived { get; set; }
+        public string DIAG_08_Derived { get; set; }
+        public string DIAG_09_Derived { get; set; }
+        public string DIAG_10_Derived { get; set; }
+        public string DIAG_11_Derived { get; set; }
+        public string DIAG_12_Derived { get; set; }
+        public string OPERTN_01_Derived { get; set; }
+        public string OPERTN_02_Derived { get; set; }
+        public string OPERTN_03_Derived { get; set; }
+        public string OPERTN_04_Derived { get; set; }
+        public string OPERTN_05_Derived { get; set; }
+        public string OPERTN_06_Derived { get; set; }
+        public string OPERTN_07_Derived { get; set; }
+        public string OPERTN_08_Derived { get; set; }
+        public string OPERTN_09_Derived { get; set; }
+        public string OPERTN_10_Derived { get; set; }
+        public string OPERTN_11_Derived { get; set; }
+        public string OPERTN_12_Derived { get; set; }
+        public string OPERTN_13_Derived { get; set; }
+        public string OPERTN_14_Derived { get; set; }
+        public string OPERTN_15_Derived { get; set; }
+        public string OPERTN_16_Derived { get; set; }
+        public string OPERTN_17_Derived { get; set; }
+        public string OPERTN_18_Derived { get; set; }
+        public string OPERTN_19_Derived { get; set; }
+        public string OPERTN_20_Derived { get; set; }
+        public string OPERTN_21_Derived { get; set; }
+        public string OPERTN_22_Derived { get; set; }
+        public string OPERTN_23_Derived { get; set; }
+        public string OPERTN_24_Derived { get; set; }
     }
 
     public class QueryClass
